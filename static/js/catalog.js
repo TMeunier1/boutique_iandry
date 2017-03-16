7dc2831a13051540f0e7264ac55ff7d8da0635e7
Tibau <xiliar1888@hotmail.fr>
2017-03-16 13:33:05 +0100

    commit

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    var GET_PARAM = function(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+    };
+
     var main = $("main");
     var divRow = $("#divrow");
     var navigation = $("<nav aria-label='Page navigation'></nav>");
@@ -13,6 +17,7 @@ $(document).ready(function() {
 
         var section = $("<a class='mainsection'></a>");
         col.append(section);
+        section.attr("href", "ficheproduit.html" + "?index=" + i);
 
         var titre = $("<h2></h2>")
         section.append(titre);
--- static/js/panier.js
+++ static/js/panier.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
     var panier = JSON.parse(localStorage.getItem("panier"));
-    for (var index in panier){
+    var prixTotal = 0;
+
+    var main = $(".panier");
+
+    for (var index in panier) {
         var produit = catalog[index]
         var quantite = panier[index];
 
-        var main = $(".panier");
         var sectionProduit = $("<section></section>");
         var divRow = $("<div class='row'></div>");
         var divCol = $("<div class='col-xs-12 textaligncenter'></div>");
@@ -13,10 +16,14 @@ $(document).ready(function() {
         var titre = $("<h2></h2>");
         var description = $("<p></p>");
         var sectionPrix = $("<section></section>");
-        var spanQuantite = $("<span></span>");
+        var quantite = $("<label>Quantitée</label>")
+        var inputQuantite = $("<input></input>");
         var spanPrix = $("<span></span>");
         var spanTVA = $("<span></span>");
-        var spanTotal = $("<span></span>");
+        var spanTotalArticle = $("<span></span>");
+        var buttonMoins = $("<button></button>");
+        var buttonPlus = $("<button></button>");
+
 
         sectionProduit.appendTo(main);
         divRow.appendTo(sectionProduit);
@@ -28,11 +35,47 @@ $(document).ready(function() {
         description.appendTo(divCol);
         description.html(catalog[index].description);
         sectionPrix.appendTo(main);
+        quantite.appendTo(sectionPrix);
+        inputQuantite.appendTo(sectionPrix);
+        inputQuantite.val(panier[index]);
+        inputQuantite.attr("class", index)
         spanPrix.appendTo(sectionPrix);
-        spanPrix.html("Prix : " + catalog[index].price + "€");
+        spanPrix.val(catalog[index].price);
+        spanPrix.html("Prix : " + spanPrix.val() + "€");
         spanTVA.appendTo(sectionPrix);
         spanTVA.html("TVA : 10%");
+        var prixArticleTotal = inputQuantite.val() * spanPrix.val();
+        spanTotalArticle.appendTo(sectionPrix);
+        spanTotalArticle.html("Total : " + prixArticleTotal + "€");
+        buttonPlus.appendTo(sectionPrix);
+        buttonPlus.html("+");
+        buttonPlus.attr("id", index)
+        buttonMoins.appendTo(sectionPrix);
+        buttonMoins.attr("id", index)
+        buttonMoins.html("-");
+
+        prixTotal = prixArticleTotal;
+        if (prixTotal != 0) {
+            prixTotal = prixTotal + prixTotal;
+        }
     }
 
 
+    buttonPlus.click(function() {
+        if ($(this).id == inputQuantite.id) {
+        var clickPlus = parseInt(inputQuantite.val());
+        clickPlus = clickPlus + 1;
+        inputQuantite.val(clickPlus);
+    }
+
+    });
+
+    buttonMoins.click(function() {
+        clickPlus = parseInt(inputQuantite.val());
+        clickPlus = clickPlus - 1;
+        inputQuantite.val(clickPlus);
+    });
+    var spanTotal = $("<span></span>");
+    spanTotal.appendTo(main);
+    spanTotal.html("Total : " + prixTotal + "€");
 });
